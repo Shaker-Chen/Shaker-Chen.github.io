80884fa8ab7116d9862affc9acb3829a28578818
Shaker-Chen <1993451899@qq.com>
2025-08-27 21:46:15 +0800

    Site updated: 2025-08-27 21:44:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2023/22/","posts/2025/4/","posts/2025/8/","posts/2024/25/","posts/2025/3/","posts/2024/111/","posts/2024/25/"];function toRandomPost(){
+var posts=["posts/2025/4/","posts/2025/8/","posts/2024/23/","posts/2025/3/","posts/2024/25/","posts/2023/22/","posts/2024/111/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
